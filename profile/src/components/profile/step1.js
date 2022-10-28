8d06002d7e6e8d2dad7d1a90fceed650c623a0d1
nguyenchung2405 <chungdaik@gmail.com>
2022-10-28 13:51:53 +0700

    edit somethings

--- profile/src/components/profile/step1.js
+++ profile/src/components/profile/step1.js
@@ -585,6 +585,11 @@ export default function SoYeuLyLich(props) {
 
     const showImage = ()=>{
         if(user_id){
+            window.addEventListener("beforeunload", function (e) {
+                var confirmationMessage = "\o/";
+                (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+                return confirmationMessage;                            //Webkit, Safari, Chrome
+            });
             return <Image />
         }
     }
