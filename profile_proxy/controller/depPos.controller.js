670478c9be569312bb9a499fa2b8f051b8bad001
nguyenchung2405 <chungdaik@gmail.com>
2023-02-27 09:40:05 +0700

    Sửa lỗi xóa user_dep_pos

--- profile_proxy/controller/depPos.controller.js
+++ profile_proxy/controller/depPos.controller.js
@@ -4,9 +4,13 @@ const local =  "http://dev.profilebe.tuoitre.vn";
 const deleteDepPos = async (req,res)=>{
     try {
         let {depPosID} = req.params;
+        let {headers: {authorization}} = req;
         const result = await axios({
             url: `${local}/user-dep-pos/${depPosID}`,
-            method: "DELETE"
+            method: "DELETE",
+            headers: {
+                Authorization: authorization
+            }
         });
         res.send(result.data);
     } catch (error) {
